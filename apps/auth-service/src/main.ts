f2b2628fcd6d9ceb50e926f08f9d31fd4dbbbc69
Jakko4real <jakko4real@gmail.com>
2025-11-25 00:54:53 +0100

    chore: changes

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,8 @@
 import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
+
+import { ErrorMiddleware } from "../../../packages/error-handler/error-middleware";
 
 const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
@@ -14,9 +17,14 @@ app.use(
   })
 );
 
+app.use(express.json({ limit: "100mb" }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
+
+app.use(ErrorMiddleware);
 
 const server = app.listen(port, host, () => {
   console.log(`Auth service is running at http://${host}:${port}`);
