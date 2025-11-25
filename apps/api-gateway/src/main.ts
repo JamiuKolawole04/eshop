00d1ad71e1b7ec6767b775ea4a06ffa051a76da9
Jakko4real <jakko4real@gmail.com>
2025-11-25 10:58:13 +0100

    fix: ci error fixes

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,8 +4,8 @@ import cors from "cors";
 import proxy from "express-http-proxy";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
-import swaggerUI from "swagger-ui-express";
-import axios from "axios";
+// import swaggerUI from "swagger-ui-express";
+// import axios from "axios";
 import cookieParser from "cookie-parser";
 
 const app = express();
