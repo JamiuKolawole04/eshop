0107f7211e55798e76067df5c8fe13d560cf971e
Jakko4real <jakko4real@gmail.com>
2025-11-27 19:09:06 +0100

    chore: making changes

---
+++ apps/auth-service/controllers/auth.controller.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export const userRegistration = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,21 @@
+import crypto from "node:crypto";
+
+import { ValidationError } from "../../../../packages/error-handler";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const validateRegistrationData = (
+  data: any,
+  userType: "user" | "seller"
+) => {
+  const { name, email, password, phone_number, country } = data;
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    (userType === "seller" && (!phone_number || !country))
+  ) {
+    throw new ValidationError("Missing required fields!");
+  }
+};
