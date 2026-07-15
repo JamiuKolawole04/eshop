17aa5ab16a164470e6ba6e04923389bd56a6548c
Jamiu Kolawole <Jakko4real@gmail.com>
2026-07-15 10:39:44 +0000

    chore(client): adding header component

--- apps/user-ui/src/app/layout.tsx
+++ apps/user-ui/src/app/layout.tsx
@@ -1,23 +1,36 @@
 // @ts-expect-error: allow importing CSS module without type declarations
-import './global.css';
-
-import Header from '@/shared/widgets/header';
-
+import "./global.css";
+import Header from "@/shared/widgets/header";
+import { Poppins, Roboto } from "next/font/google";
 
 export const metadata = {
-  title: 'Welcome to user-ui',
-  description: 'Eshop',
-}
+  title: "Welcome to user-ui",
+  description: "Eshop",
+};
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "900"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <Header />
-      <body>{children}</body>
+      <body className={`${roboto.variable} ${poppins.variable}`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- apps/user-ui/src/shared/widgets/header/header.tsx
+++ apps/user-ui/src/shared/widgets/header/header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Search } from "lucide-react";
 
 const Header = () => {
   return (
@@ -6,9 +7,24 @@ const Header = () => {
       <div className="w-[80%] py-5 m-auto flex items-center justify-between">
         <div>
           <Link href="/">
-            <span className="text-8xl font-semibold text-red-900">Eshop</span>
+            <span className="text-3xl font-semibold">Eshop</span>
           </Link>
         </div>
+        <div className="w-[50%] relative">
+          <input
+            type="text"
+            placeholder="Search for products..."
+            className="w-full px-4 font-Poppins font-medium border-[2.5px] border-[#3489ff] outline-none h-[55px]"
+          />
+
+          <div className="w-[60px] cursor-pointer flex items-center justify-center h-[55px] bg-[#3489ff] absolute top-0 right-0">
+            <Search color="#fff" />
+          </div>
+        </div>
+
+         <div className="flex items-center gap-8">
+              <Link href="/"> Profile Icon </Link>
+          </div>
       </div>
     </header>
   );
